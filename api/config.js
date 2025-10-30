bdb993050fa946566fdcb1a25a05e5a10c598457
Xc165543337 <supercalifrag.xc@gmail.com>
2025-10-30 15:53:49 +0100

    chore: credentials & configs

--- api/config.js
+++ api/config.js
@@ -1,11 +1,10 @@
-module.exports =  {
-    ACCESS_TOKEN_SECRET : "EMMA123",
-    BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
-    "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+module.exports = {
+    ACCESS_TOKEN_SECRET: 'EMMA123',
+    BDD: {
+        host: 'dpg-d41mvdgdl3ps73dimih0-a.oregon-postgres.render.com',
+        port: '5432',
+        user: 'pollution_2axp_user',
+        password: '86MfBDJ83OENI1MRD853C2ZY5cwjOgG7',
+        bdname: 'pollution_2axp'
     }
 }
-

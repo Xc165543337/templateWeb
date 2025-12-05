f75ef061607f4b32a9dc0a1044a00783d008fd53
Xc165543337 <90028194+Xc165543337@users.noreply.github.com>
2025-12-05 11:49:04 +0100

    feat: Update database configuration for new credentials

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     ACCESS_TOKEN_SECRET: 'EMMA123',
     BDD: {
-        host: 'dpg-d41mvdgdl3ps73dimih0-a.oregon-postgres.render.com',
+        host: 'dpg-d4pb1p6r433s73ej925g-a.oregon-postgres.render.com',
         port: '5432',
-        user: 'pollution_2axp_user',
-        password: '86MfBDJ83OENI1MRD853C2ZY5cwjOgG7',
-        bdname: 'pollution_2axp'
+        user: 'pollution_tp_user',
+        password: '2Y8BtEE1Uci6d6roPEyeIMUZBPxe70gm',
+        bdname: 'pollution_tp'
     }
 }

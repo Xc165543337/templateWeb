c5652cf3e195757fcfe231fdc3c0348c2ed89ae6
Xc165543337 <supercalifrag.xc@gmail.com>
2025-12-25 21:58:19 +0100

    feat: add missing pollution routes

--- api/models/index.js
+++ api/models/index.js
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize'
 import { BDD } from '../config.js'
 import utilisateursModel from './utilisateurs.model.js'
+import pollutionModel from './pollution.model.js'
 
 const sequelize = new Sequelize(`postgres://${BDD.user}:${BDD.password}@${BDD.host}/${BDD.bdname}`, {
     dialect: 'postgres',
@@ -20,5 +21,6 @@ db.Sequelize = Sequelize
 db.sequelize = sequelize
 
 db.utilisateurs = utilisateursModel(sequelize, Sequelize)
+db.pollution = pollutionModel(sequelize, Sequelize)
 
 export default db
